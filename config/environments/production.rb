a60b3ab6de4675a864026e38644d6703247e0da8
Combiz Saleh <combiz.s@gmail.com>
2013-03-22 14:31:49 -0700

    Added user registration patch1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,6 @@ Omrails::Application.configure do
   config.action_controller.perform_caching = false
 
   # In production, :host should be set to the actual host of your application.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'warm-ridge-6912.herokuapp.com' }
 
 end
